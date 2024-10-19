da4afccfb52d3ab18333564af9d1235b4298861b
nahom tewodros <nahomtewodrosm@gmail.com>
2024-10-19 19:40:51 +0300

    lets start

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="bg-pink-500 flex items-top justify-center m-[20rem]">
+      <h1>yo</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
